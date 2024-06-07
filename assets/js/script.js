ef46f7933c8b57eba7b8627a3ff533bd72949342
SinaDevCode <zigmagamesplus@gmail.com>
2024-06-08 00:29:30 +0330

    added sidebar

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,39 @@ const albumSwiper = new Swiper("[albumSwiper]", {
         prevEl: ".prev-next .prev"
     }
 });
+
+// ------ Music Playing ------->
+const music = document.querySelector('audio');
+const musicStatus = document.querySelector('[musicStatus]');
+const progress = document.querySelector('#progress');
+
+musicStatus.addEventListener('click', () => {
+    if (musicStatus.classList.contains('fa-play')) {
+        musicStatus.classList.replace('fa-play', 'fa-pause');
+        music.play();
+    }
+    else {
+        musicStatus.classList.replace('fa-pause', 'fa-play');
+        music.pause();
+        console.log(music.duration);
+    }
+});
+
+if (music.play()) {
+    setInterval(() => {
+        progress.value = music.currentTime;
+    }, 1000);
+}
+
+progress.addEventListener('change', () => {
+    music.play();
+    music.currentTime = progress.value;
+    if (musicStatus.classList.contains('fa-play')) {
+        musicStatus.classList.replace('fa-play', 'fa-pause');
+    }
+});
+
+music.onloadedmetadata = function(){
+    progress.max = music.duration
+    progress.value = music.currentTime
+}
